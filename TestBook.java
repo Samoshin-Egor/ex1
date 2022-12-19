8d06db33d125cf4a522b22e07b3406cf1800d833
Samoshin-Egor <121015765+Samoshin-Egor@users.noreply.github.com>
2022-12-19 12:27:33 +0300

    TestBook.java

---
+++ TestBook.java
@@ -0,0 +1,10 @@
+package ru.mirea.pract1_3;
+import java.lang.*;
+        public class Testbook {
+            public static void main(String [] args){
+                book a1 = new book("Тургенев Иван Сергеевич", "Отцы и дети",1862);
+                book a2 = new book( "Александр Сергеевич Пушкин", "Капианскя дочка" , 1836);
+                System.out.println(a1);
+                System.out.println (a2);
+    }
+}
